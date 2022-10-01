26e7e5d5128d60dd57700d30ec89ec39e3e1fefd
Manoj Gamachchige <manojpriyankaraucsc@gmail.com>
2022-10-02 01:41:57 +0200

    added lib example project

---
+++ example/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { UXFeedbackFloater } from 'ux-feedback-floating-button'
+
+function App() {
+  return (
+    <div>
+      <UXFeedbackFloater />
+    </div>
+  )
+}
+
+export default App
